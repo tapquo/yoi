e2ece8d8e5f75a4508b29499d538c04e8a2824c4
kikobeats <kikohumanbeatbox@gmail.com>
2014-01-17 19:12:02 +0100

    fixed load yaml file method

--- index.js
+++ index.js
@@ -8,34 +8,39 @@
 "use strict";
 
 require("coffee-script");
-require("js-yaml");
 require("colors");
+var fs = require("fs");
+var yaml = require('js-yaml');
+var path = require('path');
 
-global.config = require("../../yoi.yml");
-global.config.environment = require("../../environments/" + global.config.environment + ".yml");
+var yoi = path.join(__dirname, '../../yoi.yml');
+global.config = yaml.safeLoad(fs.readFileSync(yoi, 'utf8'));
+
+var environment = path.join(__dirname, '../../environments/' + global.config.environment + ".yml");
+global.config.environment = yaml.safeLoad(fs.readFileSync(environment, 'utf8'));
 
 var Yoi = {
     // Helpers
-    Cron        : require("./lib/helpers/cron"),
-    Model       : require("./lib/helpers/model"),
-    Rest        : require("./lib/helpers/rest"),
-    Site        : require("./lib/helpers/site"),
-    SocketTest  : require("./lib/helpers/test_socket"),
-    Test        : require("./lib/helpers/test"),
+    Cron: require("./lib/helpers/cron"),
+    Model: require("./lib/helpers/model"),
+    Rest: require("./lib/helpers/rest"),
+    Site: require("./lib/helpers/site"),
+    SocketTest: require("./lib/helpers/test_socket"),
+    Test: require("./lib/helpers/test"),
     // Services
-    Mongo       : require("./lib/services/mongo"),
-    Redis       : require("./lib/services/redis"),
-    Appnima     : require("./lib/services/appnima"),
+    Mongo: require("./lib/services/mongo"),
+    Redis: require("./lib/services/redis"),
+    Appnima: require("./lib/services/appnima"),
     // Facade
-    Mongoose    : require("mongoose"),
-    Hope        : require("hope"),
+    Mongoose: require("mongoose"),
+    Hope: require("hope"),
     // Instance
-    run         : function(callback) {
+    run: function(callback) {
         _watermark();
         return require("./lib/server").run(callback);
     },
     // Instance
-    test        : function(callback) {
+    test: function(callback) {
         _watermark();
         require("./lib/test").run();
     }
